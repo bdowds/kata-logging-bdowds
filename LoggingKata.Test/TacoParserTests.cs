7b5a51581e13c5c5da3a4ea9b05066dfc1b0375d
bdowds <bwdowds@gmail.com>
2018-04-19 13:34:17 -0500

    initial commit

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -5,17 +5,15 @@ namespace LoggingKata.Test
 {
     public class TacoParserTests
     {
-        [Fact]
-        public void ShouldDoSomething()
-        {
-            // TODO: Complete Something, if anything
-        }
-
         [Theory]
         [InlineData("Example")]
         public void ShouldParse(string str)
         {
             // TODO: Complete Should Parse
+            //Arrange
+
+            //Act
+            //Assert
         }
 
         [Theory]
@@ -24,6 +22,12 @@ namespace LoggingKata.Test
         public void ShouldFailParse(string str)
         {
             // TODO: Complete Should Fail Parse
+            //Arrange
+            var testParser = new TacoParser();
+            //Act
+            var result = testParser.Parse(str);
+            //Assert
+            Assert.Equal(null, result);
         }
     }
 }
