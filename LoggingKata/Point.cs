1534c65b08cf908e3724fa29714152e61c44c9ea
bdowds <bwdowds@gmail.com>
2018-04-23 05:02:16 -0500

    Parser set up

--- LoggingKata/Point.cs
+++ LoggingKata/Point.cs
@@ -1,8 +1,15 @@
-﻿namespace LoggingKata
+﻿using System;
+
+namespace LoggingKata
 {
     public struct Point
     {
         public double Longitude { get; set; }
         public double Latitude { get; set; }
+
+        public static implicit operator Point(double v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,16 +1,40 @@
-﻿namespace LoggingKata
+﻿using System;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the TacoBells
     /// </summary>
     public class TacoParser
     {
-        readonly ILog logger = new TacoLogger();
+        readonly ILog _logger = new TacoLogger();
         
         public ITrackable Parse(string line)
         {
-            logger.LogInfo("Begin parsing");
+            _logger.LogInfo("Begin parsing");
 
+            var cells = line.Split(',');
+
+            if (cells.Length < 2)
+            {
+                _logger.LogWarning("Missing Longitude or Latitude");
+                return null;
+            }
+
+            try
+            {
+                var longitude = double.Parse(cells[0]);
+                var latitude = double.Parse(cells[1]);
+                if (cells.Length == 3)
+                {
+                    var name = cells[2];
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Longitude or Latitude were not a valid number", e);
+                return null;
+            }
             //DO not fail if one record parsing fails, return null
             return null; //TODO Implement
         }
